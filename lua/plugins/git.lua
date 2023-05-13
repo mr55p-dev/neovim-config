6a94ad51184ebd6efe5a4ca2d148a1bb1adb1a49
Ellis Lunnon <ellislunnon@gmail.com>
2023-05-13 19:16:43 +0100

    Updating maps for fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -28,14 +28,14 @@ M.fugitive = {
 }
 
 function M.fugitive.setup()
-	vim.keymap.set('n', '<Leader>gg', ":Git ")
-	vim.keymap.set('n', '<Leader>gl', function() vim.cmd("Git lg") end, { silent = true })
-	vim.keymap.set('n', '<Leader>gs', function() vim.cmd("Git status") end, { silent = true })
+	vim.keymap.set('n', '<Leader>gg', ":Git ", { desc = "Open Git" })
+	vim.keymap.set('n', '<Leader>gl', function() vim.cmd("Git lg") end, { silent = true, desc = "Git lg" })
+	vim.keymap.set('n', '<Leader>gs', function() vim.cmd("Git status") end, { silent = true, desc = "Git status" })
 
-	vim.keymap.set('n', '<Leader>gcc', ":Git commit ")
-	vim.keymap.set('n', '<Leader>gca', function() vim.cmd("Git commit -a") end)
-	vim.keymap.set('n', '<Leader>gce', function() vim.cmd("Git commit --allow-empty") end)
-	vim.keymap.set('n', '<Leader>gcf', function() vim.cmd("Git commit --no-verify") end)
+	vim.keymap.set('n', '<Leader>gcc', ":Git commit ", { desc = "Open Git commit" })
+	vim.keymap.set('n', '<Leader>gca', function() vim.cmd("Git commit -a") end, { desc = "Commit all" })
+	vim.keymap.set('n', '<Leader>gce', function() vim.cmd("Git commit --allow-empty") end, { desc = "Commit empty" })
+	vim.keymap.set('n', '<Leader>gcf', function() vim.cmd("Git commit --no-verify") end, { desc = "Commit forced (no verify)" } )
 
 	vim.keymap.set('n', '<Leader>ga', ":Git add ")
 	vim.keymap.set('n', '<Leader>gA', function() vim.cmd("Git add .") end)
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -19,7 +19,7 @@ function M.floatterm.setup()
 	vim.keymap.set('n', '<Leader>tn',
 	function() vim.cmd('FloatermNew --height=0.7 --width=0.7 --wintype=float --name=nnn nnn') end)
 	vim.keymap.set('n', '<Leader>gt',
-	function() vim.cmd('FloatermNew --height=0.9 --width=0.9 --wintype=float --name=Git lazygit') end)
+	function() vim.cmd('FloatermNew --height=0.9 --width=0.9 --wintype=float --name=Git lazygit') end, { desc = "Open LazyGit" })
 	vim.keymap.set('n', '<Leader>ts', function () vim.cmd('FloatermNew --height=0.7 --width=0.7 --wintype=float --name=spt spt') end)
 
 	vim.keymap.set('n', '<Leader>rr', function() vim.cmd("FloatermNew --height=0.7 --width=0.7 --wintype=float --name=cargo cargo run || fish") end)
