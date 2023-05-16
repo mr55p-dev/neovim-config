81bc84a0bb78cbc97b2db40fbb17a74a1128f96b
Ellis Lunnon <ellislunnon@gmail.com>
2023-05-16 15:22:47 +0100

    Added descriptions, fixed nnp binds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,7 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set('n', '<leader>hh', function() vim.cmd'noh' end, { silent = true })
+vim.keymap.set('n', '<leader>hh', function() vim.cmd'noh' end, { silent = true, desc="Hide highlights" })
 
 
 -- Keybinds for moving between splits
@@ -15,7 +15,7 @@ vim.keymap.set({ 'n', 'v', 't' }, '<C-j>', function() vim.cmd("wincmd j") end, {
 vim.keymap.set({ 'n', 'v', 't' }, '<C-k>', function() vim.cmd("wincmd k") end, { silent = true })
 vim.keymap.set({ 'n', 'v', 't' }, '<C-l>', function() vim.cmd("wincmd l") end, { silent = true })
 
-vim.keymap.set({ 'n' }, '<C-q>', function() vim.cmd'SaveSession' vim.cmd'wqa' end, { silent = true })
+vim.keymap.set({ 'n' }, '<C-q>', ':wqa<CR>', { silent = true, desc="Quit neovim" })
 vim.keymap.set('n', '<Leader>ps', function() vim.cmd'PackerSync' end)
 vim.keymap.set('n', '<Leader>pc', function() vim.cmd'PackerCompile' end)
 
@@ -27,8 +27,7 @@ vim.keymap.set({ 'n', 'v', 'i' }, '<C-S-k>', function () vim.cmd("move-2") end, 
 vim.keymap.set({ 't' }, '<Esc>', [[<C-\><C-n>]])
 
 -- Zen mode
-vim.keymap.set('n', 'zn', function() require("zen-mode").toggle() end, { silent = true })
-
-vim.keymap.set('n', '<leader>en', function() vim.cmd('NoNeckPain') end, { silent = 'true', decription = 'Toggle no neck pain'})
-vim.keymap.set('n', '<leader>e>', function() vim.cmd('NoNeckPainWidthUp') end, { silent = 'true', decription = 'Toggle no neck pain'})
-vim.keymap.set('n', '<leader>e<', function() vim.cmd('NoNeckPainWidthDown') end, { silent = 'true', decription = 'Toggle no neck pain'})
+vim.keymap.set('n', '<leader>en', function() require("zen-mode").toggle() end, { silent = true, desc="Toggle zen mode" })
+vim.keymap.set('n', '<leader>en', function() vim.cmd('NoNeckPain') end, { silent = true, desc = 'Toggle no neck pain'})
+vim.keymap.set('n', '<leader>e>', function() vim.cmd('NoNeckPainWidthUp') end, { silent = true, desc = 'Toggle no neck pain'})
+vim.keymap.set('n', '<leader>e<', function() vim.cmd('NoNeckPainWidthDown') end, { silent = true, desc = 'Toggle no neck pain'})
