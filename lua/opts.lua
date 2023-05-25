7624746b811c704550007050257ddda884bdd4ff
Ellis Lunnon <ellislunnon@gmail.com>
2023-05-25 09:07:39 +0100

    Trying to fix the foldlevel issues

--- lua/opts.lua
+++ lua/opts.lua
@@ -23,8 +23,10 @@ vim.o.wrap = true
 -- Enable folds
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 99
+vim.opt.foldlevel = 9
+vim.opt.foldlevelstart = 9
+vim.opt.foldopen = "all"
+vim.opt.foldenable = false
 
 -- vim.o.showmode = false
 -- vim.o.cmdheight = 0
