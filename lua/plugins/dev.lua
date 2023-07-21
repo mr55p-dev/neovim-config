c4a75797f7c3c5c4e25b27bdd2ec293acd0c059c
Ellis Lunnon <ellislunnon@gmail.com>
2023-07-21 10:28:38 +0100

    Improved on jobber maps

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -4,8 +4,10 @@ return {
 		opts = {
 			layouts = {
 				["Pagemail"] = { "make -C server run", "make -C client run" },
-				["GOL UI"] = { "npm run start", "npm run ui-dev-proxy-prod" },
-				["GOL storybook"] = {"npm run start", "npm run ui-dev-proxy-prod", "npm run storybook"}
+				["GOL UI"] = { "nvm use && npm run start", "nvm use && npm run ui-dev-proxy-prod" },
+				["GOL storybook"] = { "nvm use && npm run ui-dev-proxy-prod", "nvm use && npm run storybook" },
+				["GOL all"] = { "nvm use && npm run start", "nvm use && npm run ui-dev-proxy-prod",
+					"nvm use && npm run storybook" }
 			},
 		},
 		dependencies = {
